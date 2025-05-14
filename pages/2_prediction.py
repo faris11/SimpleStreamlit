29452cf4ed17a6945a440b7718e4b522b561b0c4
M. Faris Al Hakim <farisco.totti@gmail.com>
2025-05-14 23:13:58 +0700

    Update 2_prediction.py

--- pages/2_prediction.py
+++ pages/2_prediction.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 st.set_page_config(page_title="Prediction", page_icon="📈")
 st.header("Predictions")
+
+df = pd.read_csv("model/iris.csv")
+dataset = pd.read_csv('https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv')
+
+# Tampilkan dataframe
+st.subheader("📁 Iris Dataset")
+st.dataframe(dataset)
 
 testing = st.slider("Data Testing", min_value=0, max_value=100, value=10)
 st.write(f"Nilai yang dipilih: {testing}")
