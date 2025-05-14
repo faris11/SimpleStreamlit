404f9b43fec93effa9b36ced9b99b73c9380c2c2
M. Faris Al Hakim <farisco.totti@gmail.com>
2025-05-14 14:42:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,9 @@ if name:
 # Contoh slider
 value = st.slider("Pilih nilai", 0, 100, 50)
 st.write(f"Nilai yang dipilih: {value}")
+
+df = pd.read_csv("model/iris.csv")
+
+# Tampilkan dataframe
+st.subheader("📁 Isi Data Iris")
+st.dataframe(df)
