498ac5efe45531bb199118b2529e7553254e3ba3
M. Faris Al Hakim <farisco.totti@gmail.com>
2025-05-14 23:16:43 +0700

    Update 2_prediction.py

--- pages/2_prediction.py
+++ pages/2_prediction.py
@@ -5,20 +5,27 @@ from sklearn.model_selection import train_test_split
 st.set_page_config(page_title="Prediction", page_icon="📈")
 st.header("Predictions")
 
+#Load Dataset
 df = pd.read_csv("model/iris.csv")
 dataset = pd.read_csv('https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv')
 
-# Tampilkan dataframe
+#Tampilkan Dataframe
 st.subheader("📁 Iris Dataset")
 st.dataframe(dataset)
 
+#Test Size
 testing = st.slider("Data Testing", min_value=0, max_value=100, value=10)
 st.write(f"Nilai yang dipilih: {testing}")
+t_size = testing/100
 
-# Select features and target for the iris dataset
+#Select features and target
 X = dataset.drop('variety', axis=1)
 y = dataset['variety']
 
-t_size = testing/100
-# Split the dataset into training and testing sets
+#Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=t_size, random_state=42)
+
+y_pred1 = model1.predict(X_test)
+
+#Evaluate the models
+accuracy1 = accuracy_score(y_test, y_pred1)
