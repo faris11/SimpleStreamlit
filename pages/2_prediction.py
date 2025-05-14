99b9623415577eecddc065c7ba1f3a0bbcff87a8
M. Faris Al Hakim <farisco.totti@gmail.com>
2025-05-14 23:09:38 +0700

    Update 2_prediction.py

--- pages/2_prediction.py
+++ pages/2_prediction.py
@@ -3,7 +3,13 @@ import streamlit as st
 st.set_page_config(page_title="Prediction", page_icon="📈")
 st.header("Predictions")
 
-st.markdown("#Prediction")
-
-testing = st.slider("Data Testing", min_value=10, max_value=100, value=10)
+testing = st.slider("Data Testing", min_value=0, max_value=100, value=10)
 st.write(f"Nilai yang dipilih: {testing}")
+
+# Select features and target for the iris dataset
+X = dataset.drop('variety', axis=1)
+y = dataset['variety']
+
+t_size = testing/100
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=t_size, random_state=42)
