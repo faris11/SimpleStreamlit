a45df1a2384ce849347e8b411cda96552290331a
M. Faris Al Hakim <farisco.totti@gmail.com>
2025-05-14 23:04:57 +0700

    Update 2_prediction.py

--- pages/2_prediction.py
+++ pages/2_prediction.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.set_page_config(page_title="Prediction", page_icon="📈")
-st.sidebar.header("Predictions")
+st.header("Predictions")
+
+st.markdown("#Prediction")
+
+testing = st.slider("Data Testing", min_value=10, max_value=100)
